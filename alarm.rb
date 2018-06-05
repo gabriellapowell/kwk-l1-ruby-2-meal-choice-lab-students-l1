27cc6be3db58dec99e04d422fd9b14d9cc5b5146
gabriellapowell <soccerplayer60@icloud.com>
2018-06-05 19:25:53 +0000

    automatically backed up by learn

--- alarm.rb
+++ alarm.rb
@@ -2,4 +2,8 @@
 # otherwise, if the time is between 11am between 3pm say good afternoon
 # otherwise, if the time is between 3pm to 10pm say good evening
 # otherwise, say goodnight 
+
 time.now.hour #ruby specific key word for time
+
+if time.now.hour is >= 6 && time.now.hour <= 11 puts "GOOD MORNING AMERICA!"
+  otherwise if time.now.hour 
